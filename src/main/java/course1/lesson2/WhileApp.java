ec8dc25a86d54267fb3648fdad02afe0bb70ac61
DeadLik <mc_grove@bk.ru>
2021-12-26 22:18:12 +0300

    init

---
+++ src/main/java/course1/lesson2/WhileApp.java
@@ -0,0 +1,27 @@
+package course1.lesson2;
+
+public class WhileApp {
+    public static void main(String[] args) {
+
+
+        int i = 10;
+        while (i > 0) { //maybe
+            if (i% 2 ==0) {
+                i--;
+                continue;
+            }
+            System.out.println(i);
+            i--;
+        }
+
+        /*while (true) { //infinity
+
+        }*/
+
+        int j = 10;
+        do { //at list once
+            System.out.println("j=" + j);
+            j--;
+        } while (j > 0);
+    }
+}
