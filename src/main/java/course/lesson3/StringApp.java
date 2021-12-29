d53b0f0ed228b614aced5587ae0d1dc644bdf738
DeadLik <mc_grove@bk.ru>
2021-12-29 22:26:25 +0300

    init

---
+++ src/main/java/course/lesson3/StringApp.java
@@ -0,0 +1,13 @@
+package course.lesson3;
+
+public class StringApp {
+
+    public static void main(String[] args) {
+        String str = "new String";
+        String str2 = new String("new string");
+
+
+
+        str.equals(str2);
+    }
+}
